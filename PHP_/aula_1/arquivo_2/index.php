ff08f93169e8fd64e9b13a98e94ad5242e224142
sergiogjunior <sergiogjunior2007@gmail.com>
2025-04-23 11:52:52 -0300

    .

---
+++ PHP_/aula_1/arquivo_2/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>estrutura básica</title>
+</head>
+<body>
+<?php
+
+    $nome = "Sérgio jr.";
+    $idade = "17";
+    $salario = "R$: 12,99";
+
+    // $fichacompleta = $nome." - ".$idade." - ".$salario;
+    // echo $fichacompleta;
+
+    // $fichacompleta="$nome - ";
+    // $fichacompleta .="$idade - ";
+    // $fichacompleta .="$salario";
+    // echo $fichacompleta;
+
+    echo "meu nome: {$nome} - minha idade: {$idade} - meu salario: {$salario}";
+?>
+</body>
+</html> 
---
+++ PHP_/aula_1/arquivo_3/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>estrutura básica</title>
+</head>
+<body>
+<?php
+
+    $n1 = $_GET["a"];
+    $n2 = $_GET["b"];
+
+    
+
+    echo $n1 + $n2;
+?>
+</body>
+</html> 
