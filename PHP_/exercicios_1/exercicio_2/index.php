abbb64e3d80df54bc671042502c4235cdd255d69
sergiogjunior <sergiogjunior2007@gmail.com>
2025-04-30 11:44:03 -0300

    .

---
+++ PHP_/exercicios_1/exercicio_2/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Soma Condicional</title>
+</head>
+<body>
+
+<form action="soma.php" method="get">
+    Digite o primeiro número: <input type="number" name="n1"><br>
+    Digite o segundo número: <input type="number" name="n2"><br>
+    <input type="submit" value="Calcular">
+</form>
+
+</body>
+</html>
