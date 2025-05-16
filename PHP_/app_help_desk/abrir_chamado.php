dff68fa3e5c8429452a36c62b5b126aac88c48c1
sergiogjunior <sergiogjunior2007@gmail.com>
2025-05-16 11:30:04 -0300

    .

--- PHP_/app_help_desk/abrir_chamado.php
+++ PHP_/app_help_desk/abrir_chamado.php
@@ -35,38 +35,39 @@
               <div class="row">
                 <div class="col">
                   
-                  <form>
-                    <div class="form-group">
-                      <label>Título</label>
-                      <input type="text" class="form-control" placeholder="Título">
-                    </div>
-                    
-                    <div class="form-group">
-                      <label>Categoria</label>
-                      <select class="form-control">
-                        <option>Criação Usuário</option>
-                        <option>Impressora</option>
-                        <option>Hardware</option>
-                        <option>Software</option>
-                        <option>Rede</option>
-                      </select>
-                    </div>
-                    
-                    <div class="form-group">
-                      <label>Descrição</label>
-                      <textarea class="form-control" rows="3"></textarea>
-                    </div>
+                  <form method="post" action="registra_chamado.php">
+  <div class="form-group">
+    <label>Título</label>
+    <input type="text" class="form-control" placeholder="Título" name="titulo">
+  </div>
+  
+  <div class="form-group">
+    <label>Categoria</label>
+    <select class="form-control" name="categoria">
+      <option>Criação Usuário</option>
+      <option>Impressora</option>
+      <option>Hardware</option>
+      <option>Software</option>
+      <option>Rede</option>
+    </select>
+  </div>
+  
+  <div class="form-group">
+    <label>Descrição</label>
+    <textarea class="form-control" rows="3" name="descricao"></textarea>
+  </div>
 
-                    <div class="row mt-5">
-                      <div class="col-6">
-                        <button class="btn btn-lg btn-warning btn-block" type="submit">Voltar</button>
-                      </div>
+  <div class="row mt-5">
+    <div class="col-6">
+      <button class="btn btn-lg btn-warning btn-block" type="button" onclick="window.history.back()">Voltar</button>
+    </div>
 
-                      <div class="col-6">
-                        <button class="btn btn-lg btn-info btn-block" type="submit">Abrir</button>
-                      </div>
-                    </div>
-                  </form>
+    <div class="col-6">
+      <button class="btn btn-lg btn-info btn-block" type="submit">Abrir</button>
+    </div>
+  </div>
+</form>
+
 
                 </div>
               </div>
